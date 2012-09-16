be3b1d6fbd1e0807d3790836f522428f88eb6f89
billynyh <billynyh@gmail.com>
2012-09-16 23:29:18 +0800

    use fluid layout, add sidebar

--- js/gist.js
+++ js/gist.js
@@ -22,6 +22,9 @@ function getGistUrl(id){
 function getUserUrl(user){
   return API_URL + "/users/" + user + "?callback=?";
 }
+function getStarredUrl(user){
+  return API_URL + "/users/" + user + "/starred?callback=?";
+}
 
 function loadList(_t, user, callback){
   $.getJSON(getListUrl(user), function(obj){
@@ -78,6 +81,34 @@ function loadUser(_t, user, callback){
   });
 }
 
+function loadStarred(_t, user, callback){
+  $.getJSON(getStarredUrl(user), function(obj){
+    var list = obj.data;
+    var div;
+    var n = list.length;
+    for (var i=0; i<n; i++){
+      if (i%3==0){
+        div = $('<div class="row-fluid"></div>');
+      }
+      var item = list[i];
+      var html = Handlebars.templates.gist_starred_projects(item);
+      div.append(html);
+      if (i%3==2){
+        _t.append(div);
+        div = null;
+      }
+    }
+    if (div){
+      _t.append(div);
+    }
+
+    if (n == 0){
+      _t.find("h2").hide();
+    }
+  });
+  
+}
+
 $.fn.gistLoadList = function(user, callback){
   return this.each(function(){
     var _t = $(this);
@@ -94,6 +125,11 @@ $.fn.gistLoadGist = function(id, callback){
 $.fn.gistLoadUserProfile = function(id, callback){
   return this.each(function(){
     loadUser($(this), id, callback);
+  });
+}
+$.fn.gistStarredProjects = function(id, callback){
+  return this.each(function(){
+    loadStarred($(this), id, callback);
   });
 }
 
--- js/tmpl.js
+++ js/tmpl.js
@@ -17,22 +17,44 @@ templates['gist_file_item'] = template(function (Handlebars,depth0,helpers,parti
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "source", { hash: {} }); }
   buffer += escapeExpression(stack1) + "</pre>\n</div>\n";
   return buffer;});
+templates['gist_starred_projects'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers;
+  var buffer = "", stack1, foundHelper, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
+
+
+  buffer += "<div class=\"span4\">\n    <a href=\"";
+  foundHelper = helpers.html_url;
+  stack1 = foundHelper || depth0.html_url;
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "html_url", { hash: {} }); }
+  buffer += escapeExpression(stack1) + "\">";
+  foundHelper = helpers.full_name;
+  stack1 = foundHelper || depth0.full_name;
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "full_name", { hash: {} }); }
+  buffer += escapeExpression(stack1) + "</a>\n    <p>";
+  foundHelper = helpers.description;
+  stack1 = foundHelper || depth0.description;
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "description", { hash: {} }); }
+  buffer += escapeExpression(stack1) + "</p>\n</div>\n";
+  return buffer;});
 templates['gist_user_profile'] = template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers;
   var buffer = "", stack1, foundHelper, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
 
 
-  buffer += "<div class=\"row\"><div class=\"span12\">\n  <div class=\"gist-profile well clearfix\">\n    <div class=\"profile-pic\">\n      <img src=\"";
+  buffer += "  <div class=\"gist-profile well\" >\n    <div class=\"profile-pic\">\n      <img src=\"";
   foundHelper = helpers.avatar_url;
   stack1 = foundHelper || depth0.avatar_url;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "avatar_url", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "\" class=\"img-polaroid\" />\n    </div>\n    <div class=\"span6\">\n      <div class=\"info-row clearfix\">\n        <span class=\"username\">";
+  buffer += escapeExpression(stack1) + "\" class=\"img-polaroid\" />\n    </div>\n    <div class=\"info-row\">\n      <div class=\"username\">\n        <span class=\"username\">";
   foundHelper = helpers.login;
   stack1 = foundHelper || depth0.login;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "login", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "</span>\n      </div>\n      <div class=\"info-row clearfix\">\n        <span class=\"info-label\"><i class=\"icon-user\"></i> GitHub : </span>\n        <span class=\"info-content\"><a href=\"";
+  buffer += escapeExpression(stack1) + "</span>\n      </div>\n      <div class=\"info-row\">\n        <span class=\"\">\n        <a href=\"";
   foundHelper = helpers.html_url;
   stack1 = foundHelper || depth0.html_url;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
@@ -42,7 +64,7 @@ templates['gist_user_profile'] = template(function (Handlebars,depth0,helpers,pa
   stack1 = foundHelper || depth0.html_url;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "html_url", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "</a></span>\n      </div>\n      <div class=\"info-row clearfix\">\n        <span class=\"info-label\"><i class=\"icon-thumbs-up\"></i> Public Repos : </span>\n        <span class=\"info-content\"><span class=\"badge badge-success\">";
+  buffer += escapeExpression(stack1) + "</a>\n        </span>\n      </div>\n      <div class=\"row-fluid\">\n        <span class=\"span8 info-label\"><i class=\"icon-thumbs-up\"></i> Public Repos : </span>\n        <span class=\"span4 info-content\"><span class=\"badge badge-success\">";
   foundHelper = helpers.public_repos;
   stack1 = foundHelper || depth0.public_repos;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
@@ -57,14 +79,14 @@ templates['gist_user_profile'] = template(function (Handlebars,depth0,helpers,pa
   stack1 = foundHelper || depth0.bio;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "bio", { hash: {} }); }
-  buffer += escapeExpression(stack1) + "</p>\n    </div>\n  </div>\n  <hr/>\n</div></div>\n";
+  buffer += escapeExpression(stack1) + "</p>\n    </div>\n  </div>\n";
   return buffer;});
 templates['item'] = template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers;
   var buffer = "", stack1, foundHelper, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
 
 
-  buffer += "<div class=\"row\">\n  <div class=\"span12 gist-container\">\n    <div class=\"gist-info\">\n      <h3>";
+  buffer += "<div class=\"row-fluid\">\n  <div class=\"span12 gist-container\">\n    <div class=\"gist-info\">\n      <h3>";
   foundHelper = helpers.description;
   stack1 = foundHelper || depth0.description;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
